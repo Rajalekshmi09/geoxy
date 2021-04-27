d7d44e48c9e52856764e6a353ccd54b4d61ec84a
unknown <lekshmikannan98@gmail.com>
2021-04-27 17:25:50 +0530

    adds sidebar

--- src/Scenes/Component/LeftbarComponent.jsx
+++ src/Scenes/Component/LeftbarComponent.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Button } from 'react';
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { AiFillBug, AiOutlineInfoCircle } from "react-icons/ai";
 import { DiDatabase } from "react-icons/di";
 import { TiUpload } from "react-icons/ti";
 import { FaLayerGroup, FaFilter } from "react-icons/fa";
 import { BiSliderAlt, BiLeftTopArrowCircle } from "react-icons/bi";
-
+import Grid from '@material-ui/core/Grid';
 class LeftbarComponent extends Component {
   constructor(props) {
     super(props)
@@ -24,54 +23,42 @@ class LeftbarComponent extends Component {
 
   render() {
     return (
-      <div style={{ paddingTop: '35px' }}>
-
-        <div>
-          <row>
-            <column>
-              <div class="toggle">
-                <MdKeyboardArrowRight onClick={this.toggle} />
-              </div>
-            </column>
-
-            <column>
-              {
-                this.state.visible ?
-                  <div>
-                    <div class='headerbar'>
-
-                      <h2 style={{ color: '#20a9c3' }}>geoxy</h2>
-                      <div class='header-title'>
-                        <div class="header-icon">
-                          <AiFillBug style={{ width: '20%' }} />
-                          <AiOutlineInfoCircle style={{ width: '20%' }} />
-                          <DiDatabase style={{ width: '20%' }} />
-                          <TiUpload style={{ width: '20%' }} />
-                        </div>
-                      </div>
-                      <div class="header-row">
-                        <FaLayerGroup style={{ width: '15%' }} />
-                        <FaFilter style={{ width: '15%' }} />
-                        <BiLeftTopArrowCircle style={{ width: '15%' }} />
-                        <BiSliderAlt style={{ width: '15%' }} />
-                      </div>
+      <div style={{ paddingTop: '70px' }}>
+        <Grid container spacing={1} style={{ marginLeft: '20px' }} >
+          {
+            this.state.visible ?
+              <Grid item xs={3}>
+                <div class='headerbar'>
+                  <h2 style={{ color: '#20a9c3' }}>geoxy</h2>
+                  <div class='header-title'>
+                    <div class="header-icon">
+                      <AiFillBug style={{ width: '20%' }} />
+                      <AiOutlineInfoCircle style={{ width: '20%' }} />
+                      <DiDatabase style={{ width: '20%' }} />
+                      <TiUpload style={{ width: '20%' }} />
                     </div>
-
-
-                    <div class="leftbar-body">
-                      body
-                </div>
                   </div>
-                  : []
-              }
+                  <div class="header-row">
+                    <FaLayerGroup style={{ width: '15%' }} />
+                    <FaFilter style={{ width: '15%' }} />
+                    <BiLeftTopArrowCircle style={{ width: '15%' }} />
+                    <BiSliderAlt style={{ width: '15%' }} />
+                  </div>
+                </div>
 
-            </column>
-          </row>
+                <div class="leftbar-body">
+                  body
+                </div>
+              </Grid>
+              : []
+          }
+          <Grid item xs={1} >
+            <div class="toggle" >
+              <MdKeyboardArrowRight onClick={this.toggle} />
+            </div>
+          </Grid>
+        </Grid>
 
-
-
-
-        </div>
       </div>
 
     )
--- src/Scenes/Component/MainComponent.jsx
+++ src/Scenes/Component/MainComponent.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import SidemenuComponent from './SidemenuComponent'
 import LeftbarComponent from './LeftbarComponent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
 class MainComponent extends Component {
   render() {
     return (
-      <div class="home-background">
-        <SidemenuComponent />
-        <LeftbarComponent />
+      <div >
+        <Grid container class="home-background" >
+          <Grid item >
+
+
+            <LeftbarComponent />
+            <SidemenuComponent />
+          </Grid>
+        </Grid>
       </div>
     );
   }
